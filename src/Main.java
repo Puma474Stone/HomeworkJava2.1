9a416d9463070518c192eeb042da7bb4a5f53e7b
Puma474Stone <shustov.arcadi@yandex.ru>
2024-05-28 12:28:14 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int milesPerRub = 20;
+        int bonus = ticketPrice / milesPerRub;
+
+        System.out.println("Количество начисленных бонусов " + bonus);
+
+
+
+        }
+    }
